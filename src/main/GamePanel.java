105bc5eb9d81a8ed783ff3f1332158c162e13ea1
keyjpg <93946569+keyjpg@users.noreply.github.com>
2022-04-16 16:45:02 +0200

    FPS skapad, tråd, update och render (paint) metoder skapade

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // Screen settings
     final int originalTileSize = 16; // 16x16 tile
@@ -16,13 +16,87 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels horizontalt
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels vertikalt
 
-    public GamePanel(){
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread; // tiden för spelet
+
+    // set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+
+    public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); // att göra detta true ger bättre rendering performance
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
-    /** kan va en ide att inte begränsa storleken på panelen till
-     * mappens storlek. men idk !
-     * */
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000 / FPS; // 1,000,000,000 nanosekunder
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while (gameThread != null) {
+
+            update();
+
+            repaint(); // denna kallar på paintComponent metoden
+
+            try {
+                // dessa saker säkerställer att FPS är 60 och inte 6092318492174...
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000;
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // System.out.println(FPS);
+        }
+    }
+
+    public void update() {
+        if (keyH.upPressed) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
+    }
+
+    // kan va en ide att inte begränsa storleken på panelen till mappens storlek. men idk !
 }

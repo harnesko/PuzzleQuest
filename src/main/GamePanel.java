6235776bd94c27142b9a011e80902e4fe22669e8
kristoffer leal <89843271+DonLealGUA@users.noreply.github.com>
2022-05-02 13:35:05 +0200

    Deleted unnecessary main
    unnecessary main deleted

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -43,7 +43,7 @@ public class GamePanel extends JPanel implements Runnable {
     public final int dialogState = 3;
 
     // FPS
-    int FPS = 9;
+    int FPS = 60;
 
     TileManager tileManager = new TileManager(this);
     KeyHandler keyH = new KeyHandler(this); // knapparna WASD

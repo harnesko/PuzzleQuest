112608be75c30ca7ba5b488eaf91de6bd6f0b33d
kristoffer leal <89843271+DonLealGUA@users.noreply.github.com>
2022-05-11 11:28:18 +0200

    f
    f

--- src/main/UI.java
+++ src/main/UI.java
@@ -85,7 +85,10 @@ public class UI {
             }
 
 
-            
+            if (gp.gameState == gp.optionsState ||gp.gameState == gp.noneState ) {
+                drawSettingsMenu(g2);
+                g2.fillRect(0, 0, 200, 200);
+            }
 
             if (gp.gameState == gp.dialogState) {
                 //todo later
